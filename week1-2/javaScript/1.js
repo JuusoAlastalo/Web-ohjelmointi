c53d2bbd940bf3d42fcdfcd826f22e3e95d7d49c
JuusoAlastalo <59922236+JuusoAlastalo@users.noreply.github.com>
2021-09-26 12:37:00 +0300

    js
    tehtävä1

---
+++ week1-2/javaScript/1.js
@@ -0,0 +1,51 @@
+// Tämä funktio testaa onko userInput palindromi
+// Verrataan ekaa ja vikaa kirjainta toisiinsa ja jos samat siirrytään
+// vertaamaan seuraavia. Jos kirjaimet eivät ole samat palautetaan false
+
+function checkPalindrome(str) {
+  let j = str.length - 1;
+  for (let i = 0; i < j / 2; i++) {
+    let x = str[i]; 
+    let y = str[j - i]; 
+    if (x != y) {
+     
+      return false;
+    }
+  }
+  
+  return true;
+}
+
+
+// Tämä funktio tulostaa funktion check_palindromin 
+// tuloksen mukaisen vastauksen.
+
+function isPalindrome(str) {
+
+  let ans = checkPalindrome(str);
+ 
+  if (ans == true) {
+    console.log("Antamasi userInput on palindromi");
+  } else {
+    console.log("Antamasi userInput ei ole palindromi");
+  }
+}
+
+// ***Varsinainen ohjelma alkaa tästä***
+
+console.log("`\n***Tämä ohjelma testaa onko userInput palindromi***\n"
+,"***Syöttämällä sanaksi 0 ohjelma loppuu***");
+
+while(true){
+  var input = require("readline-sync");
+  var userInput = input.question("\nAnna userInput: ");
+
+  if(userInput == 0){
+    console.log('Kiitos ohjelman käytöstä');
+    break;
+  }
+  else{
+    isPalindrome(userInput);
+  }
+  
+}
